79c1289d06deb216efe7dce2a6de2d024591456b
Andrey Arsen <a_andrey@kbtu.kz>
2024-01-24 22:01:51 +0600

    TSIS

---
+++ lab2/lists/add.py
@@ -0,0 +1,17 @@
+list1=["apple", "banana", "cherry"]
+list1.append("orange")
+print(list1)
+
+list2=["apple", "banana", "cherry"]
+list2.insert(1, "orange")
+print(list2)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+mylist=["apple", "banana", "cherry"]
+mytuple=("kiwi", "orange")
+mylist.extend(mytuple)
+print(mylist)
---
+++ lab2/lists/example1.py
@@ -0,0 +1,19 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+mylist= ["apple", "banana", "cherry", "apple", "cherry"]
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+list1=["apple", "banana", "cherry"]
+list2=[1,5,6,3]
+list3=[True, False, True]
+list4=["abc", 34, True, 40, "main"]
+print(list1, list2, list3, list4 )
+
+print(type(list4))
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
