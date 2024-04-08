579a651451f20af5e7e01e887edd413a801e7c40
Andrey Arsen <a_andrey@kbtu.kz>
2024-04-08 22:58:13 +0600

    TSIS

---
+++ lab9/Game.py
@@ -0,0 +1,150 @@
+import pygame
+import sys
+import random
+import time
+
+# Initialize Pygame
+pygame.init()
+
+# Setting up FPS
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+# Creating colors
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Other Variables for use in the program
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+INITIAL_SPEED = 2  # Initial speed
+MAX_SPEED = 15  # Maximum speed
+SPEED_INCREMENT = 0.1  # Speed increment per frame
+SCORE = 0
+
+# Setting up Fonts
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+
+# Load images
+background = pygame.image.load("AnimatedStreet.png")
+
+# Create a white screen
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Game")
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+        self.speed = INITIAL_SPEED
+
+    def move(self):
+        global SCORE
+        self.rect.move_ip(0, self.speed)
+        if self.rect.bottom > SCREEN_HEIGHT:
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+            self.speed = min(MAX_SPEED, self.speed + SPEED_INCREMENT)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.left > 0:
+            if pressed_keys[pygame.K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[pygame.K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("coin.jpeg")
+        self.image = pygame.transform.scale(self.image, (40, 40))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), -40)
+
+    def move(self):
+        self.rect.move_ip(0, 2)  # Coin gradually appears
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), -40)
+
+
+# Setting up Sprites
+P1 = Player()
+E1 = Enemy()
+
+# Creating Sprites Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+
+coin = None
+
+# Game Loop
+while True:
+
+    # Cycles through all events occurring
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    DISPLAYSURF.blit(background, (0, 0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    DISPLAYSURF.blit(scores, (10, 10))
+
+    # Moves and Re-draws all Sprites
+    for entity in all_sprites:
+        entity.move()
+        DISPLAYSURF.blit(entity.image, entity.rect)
+
+    # Add a coin if it's not already on screen
+    if not coin:
+        coin = Coin()
+        all_sprites.add(coin)
+
+    # Check for collision between player and coin
+    if coin and pygame.sprite.collide_rect(P1, coin):
+        SCORE += 10
+        coin.kill()
+        coin = None
+        # Increase enemy speed when player collects a coin
+        E1.speed += SPEED_INCREMENT
+        E1.speed = min(E1.speed, MAX_SPEED)  # Cap enemy speed
+
+    # To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('crash.wav').play()
+        time.sleep(1)
+
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+
+        pygame.display.update()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
---
+++ lab9/paint2.py
@@ -0,0 +1,115 @@
+import pygame
+
+pygame.init() # Initialize pygame
+
+painting = []
+
+timer = pygame.time.Clock() # We need it for future use with fps
+
+fps = 60 # Set Frames per second
+
+activeColor = (0, 0, 0)
+activeShape = 0
+
+w = 800 # Set Window Size
+h = 600
+
+screen = pygame.display.set_mode([w, h]) # Set Screen
+
+pygame.display.set_caption("Paint") # Set Window Title
+
+def drawDisplay():
+    pygame.draw.rect(screen, 'gray', [0, 0, w, 50]) # Draw Display
+    pygame.draw.line(screen, 'black', [0, 50], [w, 50]) # Draw Line separator
+    shapes = []
+    rect = pygame.draw.rect(screen, 'black', [5, 5, 40, 40])  # Rectangle
+    shapes.append([rect, 0])
+    circle = pygame.draw.circle(screen, 'black', [70, 25], 20)  # Circle
+    shapes.append([circle, 1])
+    equilateral_triangle = pygame.draw.polygon(screen, 'black', [[92, 5], [120, 40], [148, 5]])  # Right Triangle
+    shapes.append([equilateral_triangle, 2])
+    right_triangle = pygame.draw.polygon(screen, 'black', [[140, 40], [170, 40], [170, 5]])  # Equilateral Triangle
+    shapes.append([right_triangle, 3])
+    rhombus = pygame.draw.polygon(screen, 'black', [[180, 25], [215, 5], [250, 25], [215, 45]])  # Rhombus
+    shapes.append([rhombus, 4])
+    
+    colors=[]
+    blue = [pygame.draw.rect(screen, (0, 0, 255), [w - 35, 10, 20, 20]), (0, 0, 255)] # Draw colors
+    colors.append(blue)
+    red = [pygame.draw.rect(screen, (255, 0, 0), [w - 70, 10, 20, 20]), (255, 0, 0)] # Draw colors
+    colors.append(red)
+    green = [pygame.draw.rect(screen, (0, 255, 0), [w - 105, 10, 20, 20]), (0, 255, 0)] # Draw colors
+    colors.append(green)
+    yellow = [pygame.draw.rect(screen, (255, 255, 0), [w - 140, 10, 20, 20]), (255, 255, 0)] # Draw colors
+    colors.append(yellow)
+    black = [pygame.draw.rect(screen, (0, 0, 0), [w - 175, 10, 20, 20]), (0, 0, 0)] # Draw colors
+    colors.append(black)
+    purple = [pygame.draw.rect(screen, (255, 0, 255), [w - 205, 10, 20, 20]), (255, 0, 255)] # Draw colors
+    colors.append(purple)
+    eraser = [pygame.draw.rect(screen, (255, 255, 255), [w - 240, 10, 20, 20]), (255, 255, 255)] # Draw Eraser
+    colors.append(eraser)
+    return colors,shapes
+
+
+def drawPaint(paints):
+    for paint in paints:
+        if paint[2] == 1:
+            pygame.draw.circle(screen, paint[0], paint[1], 15)
+        elif paint[2] == 0:
+            pygame.draw.rect(screen, paint[0], [paint[1][0]-15, paint[1][1]-15, 30, 30])
+        elif paint[2] == 2:
+            pygame.draw.polygon(screen, paint[0], [[paint[1][0], paint[1][1]+15], [paint[1][0]-15, paint[1][1]-15], [paint[1][0]+15, paint[1][1]-15]])
+        elif paint[2] == 3:
+            pygame.draw.polygon(screen, paint[0], [[paint[1][0]+15, paint[1][1]+15], [paint[1][0]-15, paint[1][1]+15], [paint[1][0]+15, paint[1][1]-15]])
+        elif paint[2] == 4:
+            pygame.draw.polygon(screen, paint[0], [[paint[1][0], paint[1][1]-20], [paint[1][0]+40, paint[1][1]], [paint[1][0], paint[1][1]+20], [paint[1][0]-40, paint[1][1]]])
+
+def draw():
+    global activeColor, activeShape, mouse
+    if mouse[1] > 100:
+        if activeShape == 0:
+            pygame.draw.rect(screen, activeColor, [mouse[0]-15, mouse[1]-15, 30, 30])
+        elif activeShape == 1:
+            pygame.draw.circle(screen, activeColor, mouse, 15)
+        elif activeShape == 2:
+            pygame.draw.polygon(screen, activeColor, [[mouse[0], mouse[1]+15], [mouse[0]-15, mouse[1]-15], [mouse[0]+15, mouse[1]-15]])
+        elif activeShape == 3:
+            pygame.draw.polygon(screen, activeColor, [[mouse[0]+15, mouse[1]+15], [mouse[0]-15, mouse[1]+15], [mouse[0]+15, mouse[1]-15]])
+        elif activeShape == 4:
+            pygame.draw.polygon(screen, activeColor, [[mouse[0], mouse[1]-20], [mouse[0]+40, mouse[1]], [mouse[0], mouse[1]+20], [mouse[0]-40, mouse[1]]])
+
+run = True
+while run:
+    timer.tick(fps) # Set FPS
+    screen.fill('white') # Fill Screen
+    colors,shapes=drawDisplay()
+    mouse = pygame.mouse.get_pos() # Get Mouse Position
+    draw()
+    
+    click = pygame.mouse.get_pressed()[0] # Get Mouse Button Pressed
+    if click and mouse[1] > 100:
+        painting.append((activeColor, mouse, activeShape)) # Add Mouse Position to List
+    drawPaint(painting)
+
+    for event in pygame.event.get(): # Set quit event
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.KEYDOWN: # Set quit event
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+        if event.type == pygame.KEYDOWN: # Set quit event
+            if event.key == pygame.K_SPACE:
+                painting = []
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for i in colors:
+                if i[0].collidepoint(event.pos):
+                    activeColor = i[1]
+            for i in shapes:
+                if i[0].collidepoint(event.pos):
+                    activeShape = i[1]
+    
+
+    pygame.display.flip() # Update Screen
